84aa2b2eee9ccc4235a4939b24429a259039fdbf
giovanni0077 <nefortun94@gmail.com>
2022-09-29 15:35:46 +0300

    Task Multiply

---
+++ task03.js
@@ -0,0 +1,10 @@
+// Task Multiply
+// ============================================
+// DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+// ============================================
+
+// Solution by giovanni0077
+function multiply(a, b){
+  return a * b
+}
