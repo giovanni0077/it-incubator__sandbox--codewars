f627625268a7a548de9441c436f9054a52768e0a
giovanni0077 <nefortun94@gmail.com>
2022-09-29 15:34:06 +0300

    Task Capitalization and Mutability

---
+++ task02.js
@@ -0,0 +1,17 @@
+// Task Capitalization and Mutability
+// ============================================
+// DESCRIPTION:
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+
+// Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+// ============================================
+
+// Solution by giovanni0077
+function capitalizeWord(word) {
+  const arr = word.split('')
+  const newArr = [arr[0].toUpperCase(), ...arr.slice(1, arr.length)]
+  const str = newArr.join('')
+  return str
+}
